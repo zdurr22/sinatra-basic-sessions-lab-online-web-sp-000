a696e3f0042ef58c8d5bd233447a675a12add2c1
zdurr22 <zdurr22@gmail.com>
2021-04-09 17:41:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    configure do 
+        enable :sessions
+        set :session_secret, "popcorn"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+
+        erb :checkout
+    end
+
+
 end
